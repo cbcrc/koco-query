81b346105913d8a0095f12cedee089cb4be3c033
Maxime Séguin <info@maximeseguin.com>
2015-07-19 13:14:41 -0400

    initial commit - code mostly extracted from koco-router

---
+++ README.md
@@ -0,0 +1,2 @@
+# koco query
+
---
+++ src/query.js
@@ -0,0 +1,79 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+define(['jquery', 'jquery-deparam'],
+    function($, jQueryDeparam) {
+        'use strict';
+
+        var Query = function(url, options) {
+            var self = this;
+
+            self.options = options || {};
+            var xUrl = url || '';
+
+            var urlParts = xUrl.split('?');
+            //self.url = urlParts[0];
+
+            self.__query = '';
+            self.params = {};
+
+            if (urlParts.length > 1) {
+                self.__query = urlParts[1];
+            }
+
+            deparam(self);
+        };
+
+        Query.prototype.toString = function() {
+            return this.__query;
+        };
+
+        Query.prototype.setParam = function(name, value) {
+            var self = this;
+
+            self.__query += (getQueryStringDelimiter(self) + name + '=' + encodeURIComponent(value));
+
+            deparam(self);
+        };
+
+        Query.prototype.removeURLParameter = function(parameterName) {
+            var self = this;
+
+            var prefix = encodeURIComponent(parameterName) + '=';
+            var pars = self.__query[1].split(/[&;]/g);
+
+            //reverse iteration as may be destructive
+            for (var i = pars.length; i-- > 0;) {
+                //idiom for string.startsWith
+                if (pars[i].lastIndexOf(prefix, 0) !== -1) {
+                    pars.splice(i, 1);
+                }
+            }
+
+            if (pars.length) {
+                self.__query = '?' + pars.join('&');
+            }
+
+            self.__query = '';
+
+            deparam(self);
+        };
+
+        function getQueryStringDelimiter(self) {
+            if (self.params.length) {
+                return '&';
+            }
+
+            return '?';
+        }
+
+        function deparam(self) {
+            if (self.__query) {
+                self.params = jQueryDeparam(self.__query, self.options.coerce);
+            } else {
+                self.params = {};
+            }
+        }
+
+        return Query;
+    });
